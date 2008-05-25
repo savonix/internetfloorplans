3f138004394912bdffaa1005d7654b4d0a1561cb
Albert Lash <albert.lash@docunext.com>
2008-05-25 12:30:10 +0000

    updating to current Nexista API

--- web/index.php
+++ web/index.php
@@ -20,11 +20,9 @@ define('APP_NAME','internetfloorplans');
 $server_init = PROJECT_ROOT."/cache/".SERVER_NAME."/".APP_NAME."/".APP_NAME.".php";
 
 
-    
-    
-    
 
-if(!include($nexista_path.'/plugins/builder.php')) {
+
+if(!include($nexista_path.'/extensions/nx_builder.php')) {
     echo "Error: Unable to load server loader or builder.";
     exit;
 }
@@ -34,10 +32,10 @@ if(!include($nexista_path.'/plugins/builder.php')) {
 
 // Loader not there or manually getting rebuilt? Build it!
 if(!file_exists($server_init) || isset($_GET['nxbin'])) {
-    build_it_now();
+    nexista_build_it_now();
 } else { // Loader is there, check freshness, then either rebuild or include it.
-    check_freshness();
-        include($server_init);
-        exit;
+    nexista_check_freshness();
+    include($server_init);
+    exit;
 }
 
