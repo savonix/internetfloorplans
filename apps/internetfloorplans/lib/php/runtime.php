58e241a88cfe0c1e953bfd5ce3da280bd838f876
Albert Lash <albert.lash@docunext.com>
2008-06-01 21:50:14 +0000

    trying out new format

--- apps/internetfloorplans/lib/php/runtime.php
+++ apps/internetfloorplans/lib/php/runtime.php
@@ -1,46 +1,51 @@
 <?php
-/*
-Program: Internet Floor Plans
-Component: runtime.php
-Copyright: Savonix Corporation
-Author: Albert L. Lash, IV
-License: Gnu Affero Public License version 3
-http://www.gnu.org/licenses
+/**
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * PHP Version 5
+ *
+ * @category  Libs
+ * @package   Internet_Floor_Plans
+ * @author    Albert L. Lash, IV <albert.lash@gmail.com>
+ * @copyright Savonix Corporation
+ * @license   http://www.gnu.org/licenses Gnu Affero Public License version 3
+ * @link      http://www.internetfloorplans.com/
+ *
+ */
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program; if not, see http://www.gnu.org/licenses
-or write to the Free Software Foundation,Inc., 51 Franklin Street,
-Fifth Floor, Boston, MA 02110-1301  USA
-*/
-
-$path = $_SERVER['REQUEST_URI'];
-$path = dirname($path)."/".basename($_SERVER['SCRIPT_NAME']);
+$path        = $_SERVER['SCRIPT_NAME'];
 $path_prefix = dirname($path)."/";
-if($_GET['view_flow'] == "true" ) {
+
+if ($_GET['view_flow'] == "true" ) {
     $link_prefix = $path."?view_flow=true&nid=";
 } else {
     $link_prefix = $path."?nid=";
 }
+
 $utcdate = gmdate('Y-m-d H:i:s');
 
 
-if(is_file('../revision')) { 
+if (is_file('../revision')) {
     $svn_revision = file_get_contents('../revision');
 }
 
 
 $defaults = Nexista_Config::getSection('defaults');
-Nexista_Flow::add("defaults",$defaults,false);
+Nexista_Flow::add("defaults", $defaults, false);
 
 $runtime = array(
                 'path_prefix' => $path_prefix,
@@ -51,5 +56,5 @@ $runtime = array(
                 'svn_revision' => $svn_revision
                 );
 
-Nexista_Flow::add("runtime",$runtime,false);
+Nexista_Flow::add("runtime", $runtime, false);
 ?>
