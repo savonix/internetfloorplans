4ea5fe024424db0c21346b3535e5b9cebcb7686c
Albert Lash <albert.lash@docunext.com>
2008-06-01 02:31:03 +0000

    updated builder, fixed config typo

--- web/auth.php
+++ web/auth.php
@@ -15,16 +15,14 @@ define('APP_NAME','auth');
 
 $server_init = PROJECT_ROOT."/cache/".SERVER_NAME."/".APP_NAME."/".APP_NAME.".php";
 
-if(!include($nexista_path.'/extensions/nx_builder.php')) {
+
+if(!include($nexista_path.'/extensions/nexista_builder.php')) {
     echo "Error: Unable to load server loader or builder.";
     exit;
 }
 
-
-
-
 // Loader not there or manually getting rebuilt? Build it!
-if(!file_exists($server_init) || isset($_GET['nxbin'])) {
+if(!file_exists($server_init) || isset($_POST['x--dev--rebuild'])) {
     nexista_build_it_now();
 } else { // Loader is there, check freshness, then either rebuild or include it.
     nexista_check_freshness();
