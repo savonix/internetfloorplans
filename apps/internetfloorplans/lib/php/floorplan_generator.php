5f7989a8026b1dcfdc6f4dc3a479e64d21cea381
Albert Lash <albert.lash@docunext.com>
2009-07-24 02:59:18 +0000

    Header metadata: copyright, license, etc.

--- apps/internetfloorplans/lib/php/floorplan_generator.php
+++ apps/internetfloorplans/lib/php/floorplan_generator.php
@@ -1,6 +1,27 @@
 <?php
-// Copyright Savonix Corporation 2003-2006
+/* <!--
+Program: Internet Floor Plans
+Component: floorplan_generator.php
+Copyright: Savonix Corporation
+Author: Albert L. Lash, IV
+License: Gnu Affero Public License version 3
+http://www.gnu.org/licenses
 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, see http://www.gnu.org/licenses
+or write to the Free Software Foundation, Inc., 51 Franklin Street,
+Fifth Floor, Boston, MA 02110-1301 USA
+--> */
 
 $x_shift=0;
 // This is 3/4 the size of an 11x11 page.
